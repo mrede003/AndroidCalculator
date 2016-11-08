a647a03c7886b09cbc14038972d4964721018bb3
mredeniu <mredeniu@cs.odu.edu>
2016-11-08 17:35:32 -0500

    Starting implementing basic operation display

--- app/src/main/java/com/selftaught/mrede003/androidcalculator/MainActivity.java
+++ app/src/main/java/com/selftaught/mrede003/androidcalculator/MainActivity.java
@@ -5,7 +5,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -16,12 +18,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    public void sendMessageUpper(View view)
+    public void displayNumber(View view)
     {
-        EditText editText=(EditText) findViewById(R.id.edit_message_upper);
+        TextView mainDisplay=(TextView) findViewById(R.id.main_display);
+        TextView subDisplay=(TextView) findViewById(R.id.sub_display);
 
-        editText.setText(editText.getText().toString().toLowerCase());
+        Button b=(Button)view;
+        subDisplay.setText(b.getText().toString()+subDisplay.getText());
+        mainDisplay.setText(b.getText().toString()+mainDisplay.getText());
+    }
+    public void clear(View view)
+    {
+        TextView subDisplay=(TextView) findViewById(R.id.sub_display);
+        TextView mainDisplay=(TextView) findViewById(R.id.main_display);
+        subDisplay.setText(null);
+        mainDisplay.setText(null);
+    }
+    public void backspace(View view)
+    {
+        TextView mainDisplay=(TextView) findViewById(R.id.main_display);
+        TextView subDisplay=(TextView) findViewById(R.id.sub_display);
+        if(mainDisplay.getText().toString().equals("")) return;
+
+        mainDisplay.setText((mainDisplay.getText()
+                .toString()).substring(0, mainDisplay.getText().toString().length()-1));
+        subDisplay.setText((subDisplay.getText()
+                .toString()).substring(0, subDisplay.getText().toString().length()-1));
+    }
+    public void displayOperation(View view)
+    {
+        TextView subDisplay=(TextView) findViewById(R.id.sub_display);
+        Button b=(Button)view;
+
+        if(subDisplay.getText()!=null)
+        {
+            subDisplay.setText(subDisplay.getText()+" "+b.getText().toString());
+        }
     }
 
 }
