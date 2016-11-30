3bdfb2c7caa85de236764d2198fb45195970bf9d
mredeniu <mredeniu@cs.odu.edu>
2016-11-29 19:33:00 -0500

    Started adding Dijkstra's Shunting yard algorithm.

--- app/src/main/java/com/selftaught/mrede003/androidcalculator/MainActivity.java
+++ app/src/main/java/com/selftaught/mrede003/androidcalculator/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Stack;
+
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,8 +26,21 @@ public class MainActivity extends AppCompatActivity {
         TextView subDisplay=(TextView) findViewById(R.id.sub_display);
 
         Button b=(Button)view;
-        subDisplay.setText(b.getText().toString()+subDisplay.getText());
-        mainDisplay.setText(b.getText().toString()+mainDisplay.getText());
+        if(subDisplay.getText().toString().equals("")) {
+            mainDisplay.setText(b.getText().toString()+mainDisplay.getText().toString());//if mainDisplay is blank print char and return
+            subDisplay.setText(b.getText().toString()+subDisplay.getText().toString());
+            return;
+        }
+        if (isOperand((subDisplay.getText().toString()).substring(
+                subDisplay.getText().toString().length() - 1))) {        //if operand is at end of subDisplay
+            subDisplay.setText(subDisplay.getText().toString() +" "+
+                        (b.getText()).toString());
+            mainDisplay.setText(b.getText().toString());
+        }else {
+            subDisplay.setText(subDisplay.getText().toString() + b.getText().toString());
+            mainDisplay.setText(mainDisplay.getText().toString()+b.getText().toString());
+        }
+
     }
     public void clear(View view)
     {
@@ -33,6 +48,22 @@ public class MainActivity extends AppCompatActivity {
         TextView mainDisplay=(TextView) findViewById(R.id.main_display);
         subDisplay.setText(null);
         mainDisplay.setText(null);
+    }
+    public void equals(View view)
+    {
+        TextView subDisplay=(TextView) findViewById(R.id.sub_display);
+        String answer=subDisplay.getText().toString();
+        answer=answer.replaceAll("\\s","");
+        subDisplay.setText(answer);
+        
+    }
+    public String analyzeEquation(String equation)
+    {
+        int num1,num2;
+        num1=equation.toCharArray()[0];
+
+
+        return equation;
     }
     public void backspace(View view)
     {
@@ -50,9 +81,49 @@ public class MainActivity extends AppCompatActivity {
         TextView subDisplay=(TextView) findViewById(R.id.sub_display);
         Button b=(Button)view;
 
-        if(subDisplay.getText()!=null)
+        if(!subDisplay.getText().toString().equals("")&&!(
+                isOperand((subDisplay.getText().toString()).substring(
+                        subDisplay.getText().toString().length() - 1))))
         {
             subDisplay.setText(subDisplay.getText()+" "+b.getText().toString());
+        }
+    }
+    public boolean isOperand(String t)
+    {
+        if(t.equals(getString(R.string.divide))||
+                t.equals(getString(R.string.multiply))||
+                    t.equals(getString(R.string.add))||
+                            t.equals(getString(R.string.subtract)))
+            return true;
+        return false;
+    }
+    public static String infixToPostfix(String equation)
+    {
+        final String ops="-+/*^";
+        StringBuilder sb=new StringBuilder();
+        Stack<Integer> stack=new Stack();
+
+        for(String token: equation.split("//s"))        //split equation at space into string array
+        {                                               //for every string in the array.....
+            if(token.isEmpty())                         //if string is "" continue
+                continue;
+            char c=token.charAt(0);                     //char c is the char we're working with
+            int index=ops.indexOf(c);                   //index is the pos of the operator. is -1 if its an operand
+
+            if(index!=-1)                               //if index is an operator
+            {
+                if(stack.isEmpty())
+                {
+                    stack.push(index)
+                }else{
+                    while(!stack.isEmpty())
+                    {
+
+                    }
+                }
+            }
+
+
         }
     }
 
